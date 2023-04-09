7ab761ac018c2fe3cd2dbdd2007a457209c71bbe
Denzel <r0842959@studetn.thomasmore.be>
2023-04-09 14:49:32 +0200

    Alerts toegevoegd

---
+++ Project/ValidatieFormulier.js
@@ -0,0 +1,10 @@
+// array met foutmeldingen
+let foutmeldingen = new Array();
+
+// functie met de volledige validatie van het formulier
+function validateForm() {
+    
+    let errorAlert = document.getElementById('errorAlert')
+    let completionAlert = document.getElementById('completionAlert')
+    let paymentAlert = document.getElementById('paymentAlert')
+}
